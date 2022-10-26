f65e3ae96bfb7eb7b5afaced6c2b4cae7df0e042
jreleaserbot <jreleaser@kordamp.org>
2022-10-26 01:04:31 +0000

    mulefd v0.9.0

--- mulefd.java
+++ mulefd.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS com.javastreets.mulefd:mulefd:0.8.4
-// Generated with JReleaser 1.2.0 at 2022-10-11T02:38:33.662478198Z
+//DEPS com.javastreets.mulefd:mulefd:0.9.0
+// Generated with JReleaser 1.2.0 at 2022-10-26T01:03:32.944178251Z
 
 public class mulefd {
     public static void main(String... args) throws Exception {
